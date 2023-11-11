04fe9d10a851beb8ac1c2ca5d79d549b3524e237
azmh77 <105504132+azmh77@users.noreply.github.com>
2023-11-11 06:04:42 -0800

    Security Config

--- src/main/java/com/rhpm/testapp/modules/config/SpringSecutiryConfig.java
+++ src/main/java/com/rhpm/testapp/modules/config/SpringSecutiryConfig.java
@@ -1,30 +1,62 @@
 package com.rhpm.testapp.modules.config;
 
+import com.rhpm.testapp.modules.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SpringSecutiryConfig {
+
+
+
+    @Autowired
+    public void configureGlobal(@Lazy AuthenticationManagerBuilder auth, UserService userService, PasswordEncoder passwordEncoder) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    /*@Bean(name = "passwordEncoder")
+    public PasswordEncoder BCPasswordEncoder(){
+        return new BCryptPasswordEncoder(11);
+    }*/
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 
     @Bean
     public SecurityFilterChain security(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/", "GET")).permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(formLogin -> formLogin
                         .loginPage("/Login")
+                        .usernameParameter("email")
                         .permitAll()
                 )
                 .rememberMe(Customizer.withDefaults());
 
         return http.build();
     }
+
+
 }
 
--- src/main/java/com/rhpm/testapp/modules/service/UserService.java
+++ src/main/java/com/rhpm/testapp/modules/service/UserService.java
@@ -3,12 +3,15 @@ package com.rhpm.testapp.modules.service;
 import com.rhpm.testapp.modules.model.users.User;
 import com.rhpm.testapp.modules.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private UserRepository userRepository;
     @Autowired
@@ -22,5 +25,19 @@ public class UserService {
 
     public List<User> findAllUser(){
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        org.springframework.security.core.userdetails.User.UserBuilder builder = null;
+        User user = userRepository.findByEmail(email);
+        if (user==null) {
+            throw new UsernameNotFoundException(email);
+        }else{
+            builder = org.springframework.security.core.userdetails.User.withUsername(email);
+            builder.password(user.getPassword());
+//            builder.roles(user.getRole());
+        }
+        return builder==null ? null : builder.build();
     }
 }
