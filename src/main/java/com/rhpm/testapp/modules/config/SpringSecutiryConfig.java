8c074a8995603167f5a20d7f07b1ec3980f3ef78
azmh77 <105504132+azmh77@users.noreply.github.com>
2023-11-10 23:15:56 -0800

    SpringConfig

--- src/main/java/com/rhpm/testapp/modules/config/SpringSecutiryConfig.java
+++ src/main/java/com/rhpm/testapp/modules/config/SpringSecutiryConfig.java
@@ -1,9 +1,28 @@
 package com.rhpm.testapp.modules.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-public class SpringSecutiryConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SpringSecutiryConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/","")).permitAll()
+                        .anyRequest().authenticated())
+                .formLogin((form) -> form
+                        .loginPage("/Login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
 
 }
