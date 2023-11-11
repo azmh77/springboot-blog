243121fa275a626d5d09c23dc03d87071021ee28
azmh77 <105504132+azmh77@users.noreply.github.com>
2023-11-11 03:41:41 -0800

    spring security updata to v6

--- src/main/java/com/rhpm/testapp/modules/config/SpringSecutiryConfig.java
+++ src/main/java/com/rhpm/testapp/modules/config/SpringSecutiryConfig.java
@@ -2,33 +2,29 @@ package com.rhpm.testapp.modules.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
 @Configuration
 @EnableWebSecurity
 public class SpringSecutiryConfig {
+
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain security(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
-                        .anyRequest().authenticated())
-                .csrf(csrf -> csrf.disable())
-                .formLogin((form) -> form
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
                         .loginPage("/Login")
                         .permitAll()
                 )
-                .logout((logout) -> logout.permitAll());
+                .rememberMe(Customizer.withDefaults());
 
         return http.build();
     }
 }
+
