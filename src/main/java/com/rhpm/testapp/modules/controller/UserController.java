64579e0aa61c11c4cd2aa4a7102dd197b30fd5fd
azmh77 <105504132+azmh77@users.noreply.github.com>
2023-11-13 00:36:03 -0800

    ...

--- src/main/java/com/rhpm/testapp/modules/controller/UserController.java
+++ src/main/java/com/rhpm/testapp/modules/controller/UserController.java
@@ -1,15 +1,29 @@
 package com.rhpm.testapp.modules.controller;
 
+import com.rhpm.testapp.modules.model.posts.Category;
 import com.rhpm.testapp.modules.model.users.User;
 import com.rhpm.testapp.modules.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
-@RestController
+@Controller
 public class UserController {
     private UserService userService;
+
+    @GetMapping(value = "/users/registerUsers")
+    public String showRegisterCategory() {
+        return "registerUsers";
+    }
+
+    @PostMapping(value = "/users/registerUsers")
+    public String registerCategory(@ModelAttribute User user) throws IOException {
+        userService.registerUser(user);
+        return "redirect:/users";
+    }
 
     @Autowired
     public UserController(UserService userService) {
@@ -17,12 +31,12 @@ public class UserController {
     }
 
     @GetMapping(value = "get_users")
-    public List<User> getAllUser() {
+    public @ResponseBody List<User> getAllUser() {
         return userService.findAllUser();
     }
 
     @PostMapping(value = "create_users")
-    public User registerUser(@RequestBody User user){
+    public @ResponseBody User registerUser(@RequestBody User user){
         return userService.registerUser(user);
     }
 }
